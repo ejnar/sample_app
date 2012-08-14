c3e4718e7c4860157480bb56c08bba9b52d4e21e
Ejnar <eaakerman@gmail.com>
2012-08-14 12:58:52 +0200

    Add new test case for user email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,8 @@ describe User do
     it { should_not be_valid }
   end
 
+
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
@@ -72,6 +74,18 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
+
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -91,6 +105,8 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
+
 
   describe "return value of authenticate method" do
     before { @user.save }
